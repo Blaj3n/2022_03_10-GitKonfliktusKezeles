5e36b301a9b9ae20c85fcf204b69cbf572f5fff5
Bláj3n <95749747+Blaj3n@users.noreply.github.com>
2022-03-10 12:28:09 +0100

    2. név inicializálása

--- src/konfliktuskezeles/Konfliktuskezeles.java
+++ src/konfliktuskezeles/Konfliktuskezeles.java
@@ -2,5 +2,6 @@ package konfliktuskezeles; /*2022.03.10*/
 public class Konfliktuskezeles {
     public static void main(String[] args) {
         String nev1;
+        String nev2 = "xy";
     } 
 }
