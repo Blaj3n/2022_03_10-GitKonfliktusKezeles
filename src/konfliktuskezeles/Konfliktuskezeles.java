4e598ea632ce587377aa0f9754d1dda87ccabeb9
szf1_b <szf1_b@Tanulo-202-13.mshome.net>
2022-03-10 12:40:28 +0100

    3. név deklarálása

--- src/konfliktuskezeles/Konfliktuskezeles.java
+++ src/konfliktuskezeles/Konfliktuskezeles.java
@@ -1,7 +1,9 @@
 package konfliktuskezeles; /*2022.03.10*/
 public class Konfliktuskezeles {
     public static void main(String[] args) {
-        String nev1;
-        String nev2 = "xy";
+        int i = 7;
+        String nev1 = "UB";
+        String nev2 = "X.Y.";
+        int nev3;
     } 
 }
